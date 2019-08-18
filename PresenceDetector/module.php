ca56a407ec2b4031541536cb1e8e1aef40cbe8ef
Heiko Wilknitz <heiko.wilknitz@wilkware.de>
2019-08-18 17:18:18 +0200

    Initial commit

---
+++ PresenceDetector/module.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once __DIR__.'/../libs/traits.php';  // Allgemeine Funktionen
+
+// CLASS PresenceDetector
+class PresenceDetector extends IPSModule
+{
+    use DebugHelper;
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->RegisterPropertyInteger('MotionVariable', 0);
+        $this->RegisterPropertyInteger('BrightnessVariable', 0);
+        $this->RegisterPropertyInteger('ThresholdValue', 0);
+        $this->RegisterPropertyInteger('SwitchVariable', 0);
+        $this->RegisterPropertyInteger('ScriptVariable', 0);
+        $this->RegisterPropertyBoolean('OnlyBool', false);
+    }
+
+    public function ApplyChanges()
+    {
+        if ($this->ReadPropertyInteger('MotionVariable') != 0) {
+            $this->UnregisterMessage($this->ReadPropertyInteger('MotionVariable'), VM_UPDATE);
+        }
+        //Never delete this line!
+        parent::ApplyChanges();
+        //Create our trigger
+        if (IPS_VariableExists($this->ReadPropertyInteger('MotionVariable'))) {
+            $this->RegisterMessage($this->ReadPropertyInteger('MotionVariable'), VM_UPDATE);
+        }
+    }
+
+    /**
+     * Interne Funktion des SDK.
+     * data[0] = neuer Wert
+     * data[1] = wurde Wert ge�ndert?
+     * data[2] = alter Wert
+     * data[3] = Timestamp.
+     */
+    public function MessageSink($timeStamp, $senderID, $message, $data)
+    {
+        // $this->SendDebug('MessageSink', 'SenderId: '. $senderID . 'Data: ' . print_r($data, true), 0);
+        switch ($message) {
+            case VM_UPDATE:
+                if ($senderID != $this->ReadPropertyInteger('MotionVariable')) {
+                    // Safety Check
+                    $this->SendDebug('MessageSink', $senderID.' unbekannt!');
+                    break;
+                }
+                if ($data[0] == true && $data[1] == true) { // OnChange auf TRUE, d.h. Bewegung erkannt
+                    $this->SendDebug('MessageSink', 'OnChange auf TRUE - Bewegung erkannt');
+                    $this->SwitchState();
+                } elseif ($data[0] == false && $data[1] == true) { // OnChange auf FALSE, d.h. keine Bewegung
+                    $this->SendDebug('MessageSink', 'OnChange auf FALSE - keine Bewegung');
+                } else { // OnChange auf FALSE, d.h. keine Bewegung
+                    $this->SendDebug('MessageSink', 'OnChange unveraendert - keine Zustandsaenderung');
+                }
+            break;
+        }
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TPD_SwitchState($id);
+     */
+    public function SwitchState()
+    {
+        // Check Brightness
+        if ($this->ReadPropertyInteger('BrightnessVariable') != 0) {
+            $bv = GetValue($this->ReadPropertyInteger('BrightnessVariable'));
+            $tv = $this->ReadPropertyInteger('ThresholdValue');
+            if ($tv != 0 && $bv > $tv) {
+                $this->SendDebug('SwitchState', 'Oberhalb Schwellwert: '.$bv.'(Schwellwert: '.$tv.')');
+
+                return; // nix zu tun
+            }
+            $this->SendDebug('SwitchState', 'Immer oder unterhalb Schwellwert: '.$bv.' (Schwellwert: '.$tv.')');
+        }
+        // Variable schalten
+        if ($this->ReadPropertyInteger('SwitchVariable') != 0) {
+            $sv = $this->ReadPropertyInteger('SwitchVariable');
+            if ($this->ReadPropertyBoolean('OnlyBool') == true) {
+                SetValueBoolean($sv, true);
+            } else {
+                //$pid = IPS_GetParent($sv);
+                //$ret = @HM_WriteValueBoolean($pid, 'STATE', true); //Ger�t einschalten
+                $ret = @RequestAction($sv, true); //Ger�t einschalten
+                if ($ret === false) {
+                    $this->SendDebug('SwitchState', 'Ger�t konnte nicht eingeschalten werden (UNREACH)!');
+                }
+            }
+            $this->SendDebug('SwitchState', 'Variable (#'.$sv.') auf true geschalten!');
+        }
+        // Script ausf�hren
+        if ($this->ReadPropertyInteger('ScriptVariable') != 0) {
+            if (IPS_ScriptExists($this->ReadPropertyInteger('ScriptVariable'))) {
+                $sr = IPS_RunScript($this->ReadPropertyInteger('ScriptVariable'));
+                $this->SendDebug('SwitchState', 'Script Return Value: '.$rs);
+            }
+        }
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * TPD_SetThreshold($id, $threshold);
+     *
+     * @param bool $threshold Helligkeitsschwellwert ab welchem geschalten werden soll.
+     *
+     * @return bool true if successful, otherwise false.
+     */
+    public function SetThreshold(int $threshold)
+    {
+        if ((($threshold % 5) == 0) && $threshold >= 0 && $threshold <= 50 || $threshold = 75 || $threshold = 100) {
+            IPS_SetProperty($this->InstanceID, 'ThresholdValue', $threshold);
+            IPS_ApplyChanges($this->InstanceID);
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,106 @@
+[![Version](https://img.shields.io/badge/Symcon-PHP--Modul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Product](https://img.shields.io/badge/Symcon%20Version-4.1%20%3E-blue.svg)](https://www.symcon.de/produkt/)
+[![Version](https://img.shields.io/badge/Modul%20Version-1.2.20190818-orange.svg)](https://github.com/Wilkware/IPSymconToolmatic)
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![StyleCI](https://github.styleci.io/repos/76893952/shield?style=flat)](https://github.styleci.io/repos/76893952)
+
+# Präsenzmelder
+
+Schaltet in Abhängikeit von Bewegung und Helligkeit hinterlegte Variable bzw. führt ein Script aus.
+
+## Inhaltverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Installation](#3-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#5-statusvariablen-und-profile)
+6. [WebFront](#6-webfront)
+7. [PHP-Befehlsreferenz](#7-php-befehlsreferenz)
+8. [Versionshistorie](#8-versionshistorie)
+
+### 1. Funktionsumfang
+
+* Übernimmt die Bewewgungsdaten vom Melder und verarbeitet bzw. reicht sie weiter.
+* Einstellung eines Helligkeitswertes, ab welchem weiterverarbeitet werden soll.
+* Zusätzlich bzw. ausschließlich kann ein Script ausgeführt werden.
+* Über die Funktion _TPD_SetThreshold(id, wert)_ kann der Schwellwert der Helligkeit gesetzt werden.
+
+### 2. Voraussetzungen
+
+* IP-Symcon ab Version 4.1
+
+### 3. Software-Installation
+
+* Über den Modul Store das Modul 'Presence Detector' installieren.
+* Alternativ Über das Modul-Control folgende URL hinzufügen.  
+`https://github.com/Wilkware/IPSymconPresenceDetector` oder `git://github.com/Wilkware/IPSymconPresenceDetector.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+* Unter "Instanz hinzufügen" ist das 'Präsensmelder'-Modul (Alias: Bewegungsmelder) unter dem Hersteller '(Sonstige)' aufgeführt.
+
+__Konfigurationsseite__:
+
+Name               | Beschreibung
+------------------ | ---------------------------------
+MotionVariable     | Statusvariable eines Bewegungsmelders (true = Anwesend; false = Abwesend).
+BrightnessVariable | Quellvariable, über welche die Helligkeit abgefragt werden kann, bei HmIP-SMI ist es ILLUMINATION.
+ThresholdValue     | Schwellwert, von 0 bis 100 Lux.
+SwitchVariable     | Zielvariable, die bei hinreichender Bedingung geschalten wird (true). 
+ScriptVariable     | Script(auswahl), welches zum Einsatz kommen soll.
+OnlyBool           | Schalter, ob die Statusvariable über HM-Befehl geschaltet werden soll oder einfach ein nur einfacher boolscher Switch gemacht werden soll.
+
+### 5. Statusvariablen und Profile
+
+Es werden keine zusätzlichen Profile benötigt.
+
+### 6. WebFront
+
+Es ist keine weitere Steuerung oder gesonderte Darstellung integriert.
+
+### 7. PHP-Befehlsreferenz
+
+`void TPD_SwitchState(int $InstanzID);`  
+Schaltet bei hinreichender Bedingung die Schaltervariable an.  
+Die Funktion liefert keinerlei Rückgabewert.  
+Direkter Aufruf macht aber eigentlich kein Sinn.  
+
+Beispiel:  
+`TPD_SwitchState(12345);`  
+
+`void TPD_SetThreshold(int $InstanzID, int wert);`  
+Setzt den Helligkeits-Schwellwert auf den neuen Lux-'wert'.  
+Die Funktion liefert true im Erfolgsfall.  
+
+Beispiel:  
+`TPD_SetThreshold(12345, 50);`  
+Setzt den Schwellwert auf 50 Lux.
+
+### 8. Versionshistorie
+
+v1.2.20190818
+
+* _NEU_: Umstellung für Module Store
+* _FIX_: Dokumentation überarbeitet
+
+v1.1.20170322
+
+* _FIX_: Anpassungen für IPS Version 5
+
+v1.0.20170125
+
+* _NEU_: Initialversion
+
+## Entwickler
+
+* Heiko Wilknitz ([@wilkware](https://github.com/wilkware))
+
+## Spenden
+
+Die Software ist für die nicht kommzerielle Nutzung kostenlos, Schenkungen als Unterstützung für den Entwickler bitte hier:  
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8816166" target="_blank"><img src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" /></a>
+
+## Lizenz
+
+[![Licence](https://licensebuttons.net/i/l/by-nc-sa/transparent/00/00/00/88x31-e.png)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
